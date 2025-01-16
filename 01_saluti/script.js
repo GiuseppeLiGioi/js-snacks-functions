ed88a7217d649824e52af01f8bb8a2fabbcd64d0
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-16 12:24:52 +0100

    CREATE: add folders on workspace

---
+++ 01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function Saluti(string1){
+
+return console.log(`Ciao ${string1}`);
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+Saluti (userName);
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
