4a42e63fff24dd57159de5cacf418050531b29a7
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-16 15:22:48 +0100

    ADD. fourth exercise. normal and arrow function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,40 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+let contaVocali = 0;
+let allVocali = "aeiou"
 
 
 // Dichiara la funzione qui.
+function numeroVocali(parola){
+for(let i = 0; i < parola.length; i++){    
+if (allVocali.includes(parola[i])){
+    contaVocali=contaVocali+1;
+}
 
+}
+
+return console.log(`il numero di vocali nella parola ${parola} sono: ${contaVocali}`);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+numeroVocali(word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
+//arrow function
+let numVocali = (parola1) => {
+    for (let i = 0; i < parola1.length; i++ ){
+    if(allVocali.includes(parola1[i])){
+        contaVocali+=1;
+
+    }    
+    }
+    return console.log(`il numero di vocali nella parola ${parola1} sono: ${contaVocali}`);
+}
+
+//invoco arrow function
+numVocali(word);
